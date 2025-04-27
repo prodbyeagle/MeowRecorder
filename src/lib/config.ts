dcbf845c6b69a74bb2be8ab54b0cffa819f3e6a7
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-27 22:57:52 +0200

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,11 +2,8 @@ import type { ColorResolvable } from 'discord.js';
 
 export const config = {
 	token: process.env.BOT_TOKEN || 'discord_bot_key',
-	supabaseUrl: process.env.SUPABASE_URL || 'supabase_key',
-	supabaseKey: process.env.ANON_KEY || 'anon_key',
-	databaseUrl: process.env.DATABASE_URL || 'database_url',
 	dev: true,
-	recordingChannel: '1365111820104503356',
+	recordingChannel: '1365111820104503356', // channel where the recording will be sent. (large files not)
 };
 
 //? all colors for embeds.
